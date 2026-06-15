18bdf1af3d79c6dfd0ab6f276a17e68682658f27
unmukt1208 <unmuktmishra1208@gmail.com>
2026-06-15 16:28:22 +0200

    Your message

---
+++ user-input.py
@@ -0,0 +1,6 @@
+name = input('Enter your name:')
+grade = input('What grade are you in?:')
+city = input('What city do you live in?:')
+favorite_subject = input('What is your favorite subject?:')
+
+print(f'Hello {name}, nice to meet you. Your grade level is {grade} and your city of residence is {city}. Your favorite subject is {favorite_subject}.')
