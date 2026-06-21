b5635b0b16af98144aa3d54e5d13934916d823d2
unmukt1208 <unmuktmishra1208@gmail.com>
2026-06-21 19:15:44 +0200

    Your message

---
+++ acp-user-input.py
@@ -0,0 +1,7 @@
+birthday1 = input('Enter your Birthdate(DD/MM/YYYY):')
+birthday2 = input('Enter your Birthdate(DD/MM/YYYY):')
+birthday3 = input('Enter your Birthdate(DD/MM/YYYY):')
+birthday4 = input('Enter your Birthdate(DD/MM/YYYY):')
+birthday5 = input('Enter your Birthdate(DD/MM/YYYY):')
+
+print(f'Hello, my first friends birthday is {birthday1}, my second friends birthday is {birthday2}, my third friends birthday is {birthday3}, my fourth friends birthday is {birthday4}, my fifth friends birthday is {birthday5}.')
