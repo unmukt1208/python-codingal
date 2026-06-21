f42700319585288f1fe885cb2a98c33b6e2ed8e1
unmukt1208 <unmuktmishra1208@gmail.com>
2026-06-21 19:45:01 +0200

    Your message

---
+++ data-types.py
@@ -0,0 +1,6 @@
+name = input('Enter your name:')
+grade = int (input('What grade are you in?:'))
+age = int (input('How old are you?:'))
+weight = float (input('How much do you weigh?:'))
+
+print(f'Data type of {name} is {type(name)}, data type of {grade} is {type(grade)}, data type of {age} is {type(age)}, data type of {weight} is {type(weight)}.')
